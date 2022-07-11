f3560f18647326521d97c70a3720b15623b1380a
tejass511 <tejas511ramteke@gmail.com>
2022-07-12 00:05:23 +0530

    Completed Teams Model

--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
+from .models import Teams
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/home.html')
+    teamss=Teams.objects.all()
+    data={
+        'teamss':teamss,
+    }
+    return render(request, 'pages/home.html',data)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    teamss=Teams.objects.all()
+    data={
+        'teamss':teamss
+    }
+    return render(request, 'pages/about.html',data)
 
 def services(request):
     return render(request, 'pages/services.html')
 
 def contact(request):
     return render(request, 'pages/contact.html')
-        
